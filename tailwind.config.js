bc7e051c4cfe445b7ba93dbe0b84811acd1eaabe
Soumil Datta <sdatta2@cs.olemiss.edu>
2021-02-14 18:17:30 -0600

    Create new nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      primaryBlue: {
+        DEFAULT: "#428FD7",
+      },
+    },
     extend: {},
   },
   variants: {
