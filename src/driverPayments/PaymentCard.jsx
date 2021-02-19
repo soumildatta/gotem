e5e2b1bfd0847ef0946d831b738f50e62cb0edcf
Soumil Datta <sdatta2@cs.olemiss.edu>
2021-02-18 19:29:15 -0600

    Feed info to payment cards through props

--- src/driverPayments/PaymentCard.jsx
+++ src/driverPayments/PaymentCard.jsx
@@ -1,26 +1,28 @@
 import React from "react";
 
-const PaymentCard = () => {
+const PaymentCard = ({ data }) => {
   return (
     <tr>
       <td className="px-8 py-4">
         <div className="flex items-center">
           <div>
-            <div className="text-md font-medium text-gray-900">Jane Cooper</div>
+            <div className="text-md font-medium text-gray-900">{data.name}</div>
             <div className="text-sm text-gray-500">
-              <strong>To: </strong>Baptist Memorial Hospital, MS 38655
+              <strong>To: </strong>
+              {data.to}
             </div>
             <div className="text-sm text-gray-500">
-              <strong>From: </strong>1253 Mansion House Manor, Poor Place, MS
+              <strong>From: </strong>
+              {data.from}
             </div>
           </div>
         </div>
       </td>
       <td className="px-8 py-4 text-right whitespace-nowrap">
         <span className="px-2 py-1 mb-1 inline-flex text-xl leading-5 font-semibold rounded-full bg-green-100 text-green-800 ">
-          $4.00
+          ${data.earnings}
         </span>
-        <div className="text-sm text-gray-500">24/32/3516</div>
+        <div className="text-sm text-gray-500">{data.date}</div>
       </td>
     </tr>
   );
