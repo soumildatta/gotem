871433fd03a757b75c0b2b9e92f178e011d8082a
Thong Tra <thtra@go.olemiss.edu>
2021-03-11 18:53:40 -0600

    Started UserDashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import "./App.css";
 import Navbar from "./shared/Navbar/Navbar";
 import DriverPayments from "./driverPayments/DriverPayments";
 import Requests from "./requests/Requests";
+import UserDashboard from "./userDashboard/UserDashboard";
 function App() {
   return (
     <div className="App overflow-x-hidden overflow-y-hidden">
       <Navbar />
       <div className="overflow-x-hidden overflow-y-hidden">
-        <DriverPayments />
-        <Requests />
+        {/* <DriverPayments />
+        <Requests /> */}
+        <UserDashboard />
       </div>
     </div>
   );
---
+++ src/userDashboard/UserDashboard.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import UserRequestCards from "./UserRequestCards";
+
+const UserDashboard = () => {
+  return (
+    <div class="flex flex-col">
+      <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table class="min-w-full divide-y divide-gray-200">
+              <thead class="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    class="px-8 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Name
+                  </th>
+                  <th
+                    scope="col"
+                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Address
+                  </th>
+                  <th
+                    scope="col"
+                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Status
+                  </th>
+                  <th scope="col" class="relative px-6 py-3">
+                    <span class="sr-only">Options</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody class="bg-white divide-y divide-gray-200">
+                <UserRequestCards />
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserDashboard;
