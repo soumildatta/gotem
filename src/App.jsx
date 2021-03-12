794442560faeec288cbdd62342813a958e4cca9a
Maryfrances Umeora <mumeora@u.rochester.edu>
2021-03-11 19:48:13 -0600

    hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import "./App.css";
 import Navbar from "./shared/Navbar/Navbar";
+import Signup from "./login/signup";
+import Signin from "./login/signin"
 import DriverPayments from "./driverPayments/DriverPayments";
 import Requests from "./requests/Requests";
+
 function App() {
   return (
     <div className="App overflow-x-hidden overflow-y-hidden">
       <Navbar />
+      <Signup/>
+      <Signin/>
+
       <div className="overflow-x-hidden overflow-y-hidden">
         <DriverPayments />
         <Requests />
