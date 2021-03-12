cbb594ed7e9d25719dc80332b6c5d7c8112a43f4
Soumil Datta <soumildatta@gmail.com>
2021-03-11 19:11:02 -0600

    Fixed alignment for price items and make page responsive

--- src/userDashboard/UserDashboard.jsx
+++ src/userDashboard/UserDashboard.jsx
@@ -18,15 +18,9 @@ const UserDashboard = () => {
                   </th>
                   <th
                     scope="col"
-                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    class="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider"
                   >
                     Price
-                  </th>
-                  <th
-                    scope="col"
-                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Destination
                   </th>
                 </tr>
               </thead>
--- src/userDashboard/UserRequestCards.jsx
+++ src/userDashboard/UserRequestCards.jsx
@@ -6,23 +6,21 @@ const UserRequestCards = () => {
       <td className="px-8 py-4">
         <div className="flex items-center">
           <div>
-            <div className="text-md font-medium text-gray-900">TEMP</div>
-            <div className="text-sm text-gray-500">
-              <strong>To: </strong>
-              TEMP
+            <div className="text-md font-medium text-gray-900">
+              Driver: Soumil
             </div>
             <div className="text-sm text-gray-500">
-              <strong>From: </strong>
-              TEMP
+              <strong>To: </strong>
+              Baptist Memorial Hospital
             </div>
           </div>
         </div>
       </td>
-      <td className="px-6 py-4 whitespace-nowrap">
+      <td className="px-6 py-4 text-right whitespace-nowrap">
         <span className="px-2 py-1 mb-1 inline-flex text-xl leading-5 font-semibold rounded-full bg-green-100 text-green-800 ">
-          TEMP
+          $100
         </span>
-        <div className="text-sm text-gray-500">TEMP</div>
+        <div className="text-sm text-gray-500">3/12/2021</div>
       </td>
     </tr>
   );
