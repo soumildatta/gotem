fbb99291114323f5b32c2f7fbb47568061801c4f
Soumil Datta <soumildatta@gmail.com>
2021-03-11 19:44:34 -0600

    Change navbar logo

--- src/userDashboard/UserDashboard.jsx
+++ src/userDashboard/UserDashboard.jsx
@@ -3,9 +3,12 @@ import UserRequestCards from "./UserRequestCards";
 import CurrentRequest from "./CurrentRequest";
 
 const UserDashboard = () => {
+  const ride = true;
+
   return (
     <div>
-      <CurrentRequest />
+      {ride ? <CurrentRequest /> : <h1>Hello</h1>}
+      {/* <CurrentRequest /> */}
       <div class="flex flex-col">
         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
