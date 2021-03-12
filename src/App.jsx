4372acd251253eb318eacb743884fff27ff3622d
william amminger <wamminge@go.olemiss.edu>
2021-03-12 14:19:41 -0600

    Created request ride form page for users

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./shared/Navbar/Navbar";
+import RequestRide from "./requestRide/RequestRide";
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar/>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Navbar/>
+			<RequestRide />
+		</div>
+	);
 }
 
 export default App;
