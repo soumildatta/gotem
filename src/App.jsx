7725b1a977b2d9e68e4b9b4c0603a15f8e820c14
Maryfrances Umeora <mumeora@u.rochester.edu>
2021-03-18 18:36:30 -0500

    routing v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
 import "./App.css";
-import Navbar from "./shared/Navbar/Navbar";
 import Signup from "./login/signup";
 import Signin from "./login/signin"
-import DriverPayments from "./driverPayments/DriverPayments";
 import Requests from "./requests/Requests";
+import Navbar from "./shared/Navbar/Navbar";
+import DriverPayments from "./driverPayments/DriverPayments";
+
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App overflow-x-hidden overflow-y-hidden">
-      <Navbar />
-      <Signup/>
-      <Signin/>
 
-      <div className="overflow-x-hidden overflow-y-hidden">
-        <DriverPayments />
-        <Requests />
+    <Router>
+
+      <div className="App overflow-x-hidden overflow-y-hidden">
+        <Navbar />
       </div>
-    </div>
+
+      <Switch>
+        {/*<Route exact path="/" component={Home}/>*/}
+        <Route path="/signup" component={Signup}/>
+        <Route path="/signin" component={Signin}/>
+        <Route path="/requests" component={Requests}/>
+        <Route path="/payment-history" component={DriverPayments}/>
+      </Switch>
+
+    </Router>
   );
 }
 
