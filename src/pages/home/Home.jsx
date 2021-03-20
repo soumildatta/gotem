4b1b02534401f638324b37c99f749ed60271d42b
Farid Salau <fsalau1@go.olemiss.edu>
2021-03-20 00:00:22 -0500

    Add homepage

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  // changes the <html> bgColor on mount
+  useEffect(() => {
+    document.documentElement.style.backgroundColor = "#428FD7";
+  }, []);
+  return (
+    <div className="relative bg-primaryBlue overflow-hidden">
+      <div className="max-w-7xl mx-auto">
+        <div className="relative z-10 pb-8 bg-primaryBlue sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+          <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+            <div className="sm:text-center lg:text-left">
+              <h1 className="text-4xl tracking-tight font-extrabold text-white sm:text-5xl md:text-6xl">
+                <span className="block xl:inline">
+                  Save your life, please don't die
+                </span>
+              </h1>
+              <p className="mt-3 text-base text-white sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+                lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
+                fugiat aliqua.
+              </p>
+              <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+                <div className="rounded-md shadow">
+                  <Link
+                    to="/signup"
+                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-primaryBlue bg-white md:py-4 md:text-lg md:px-10"
+                  >
+                    Get Started
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </main>
+        </div>
+      </div>
+      <div className="lg:absolute lg:inset-y-0 lg:right-0">
+        <img
+          className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+          src="https://images.unsplash.com/photo-1519494026892-80bbd2d6fd0d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
