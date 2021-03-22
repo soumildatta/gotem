0bb00d39f7f012cac7f7e85698507fd8d885374d
Thong Tra <thtra@go.olemiss.edu>
2021-03-22 18:49:39 -0500

    changed class to className and switched status to time

--- src/pages/requests/RequestCards.jsx
+++ src/pages/requests/RequestCards.jsx
@@ -4,35 +4,35 @@ import React from "react";
 const RequestCards = ({ data }) => {
   return (
     <tr>
-      <td class="px-4 py-4 whitespace-nowrap">
-        <div class="flex items-center">
-          <div class="ml-4">
-            <div class="text-sm font-medium text-gray-900">{data.name}</div>
-            <div class="text-sm text-gray-500">{data.info}</div>
+      <td className="px-4 py-4 whitespace-nowrap">
+        <div className="flex items-center">
+          <div className="ml-4">
+            <div className="text-sm font-medium text-gray-900">{data.name}</div>
+            <div className="text-sm text-gray-500">{data.info}</div>
           </div>
         </div>
       </td>
-      <td class="px-6 py-4 whitespace-nowrap">
-        <div class="text-sm text-gray-900">{data.address}</div>
+      <td className="px-6 py-4 whitespace-nowrap">
+        <div className="text-sm text-gray-900">{data.address}</div>
       </td>
-      <td class="px-6 py-4 whitespace-nowrap">
+      <td className="px-6 py-4 whitespace-nowrap">
         {/* these are the same span, prolly better if we make things like this reusable considering we don't know what we'll be building in the future */}
         {/* also, best practice is to break this logic out into a function above and pass data.status as a param returning the correct span */}
-        {data.status === "Emergency" ? (
-          <span class="px-4 py-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-500 text-white">
+        {/* {data.status === "Emergency" ? (
+          <span className="px-4 py-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-500 text-white">
             {data.status}
           </span>
-        ) : (
-          <span class="px-4 py-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-500 text-white">
-            {data.status}
-          </span>
-        )}
+        ) : ( */}
+        <span className="px-4 py-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-500 text-white">
+          {data.time}
+        </span>
+        {/* )} */}
       </td>
-      <td class="px-6 py-4 whitespace-nowrap">
-        <button class="px-8 py-4 inline-flex text-s leading-5 font-semibold rounded-full bg-green-100 hover:bg-green-200 text-green-800 mr-3">
+      <td className="px-6 py-4 whitespace-nowrap">
+        <button className="px-8 py-4 inline-flex text-s leading-5 font-semibold rounded-full bg-green-100 hover:bg-green-200 text-green-800 mr-3">
           Accept
         </button>
-        <button class="px-8 py-4 inline-flex text-s leading-5 font-semibold rounded-full bg-red-100 hover:bg-red-200 text-red-800">
+        <button className="px-8 py-4 inline-flex text-s leading-5 font-semibold rounded-full bg-red-100 hover:bg-red-200 text-red-800">
           Decline
         </button>
       </td>
--- src/pages/requests/Requests.jsx
+++ src/pages/requests/Requests.jsx
@@ -6,53 +6,53 @@ const Requests = () => {
   const requestsData = [
     {
       name: "Jane Cooper",
-      info: "Yikes, not good",
+      info: "Monthly Checkup",
       address: "3598 Mansion House Manor, Poor Place, MS 38655",
-      status: "Emergency",
+      time: "12:30 PM",
     },
     {
       name: "Joe Cooper",
-      info: "Ehh, aight.",
+      info: "Visit",
       address: "3598 Mansion House Manor, Poor Place, MS 38655",
-      status: "Normal",
+      time: "2:00 PM",
     },
   ];
   return (
-    <div class="flex flex-col">
+    <div className="flex flex-col">
       <h2 className="mt-6 text-center text-5xl font-medium ml-3 mb-4 text-gray-900 flex justify-left ">
         Requests
       </h2>
-      <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-          <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-            <table class="min-w-full divide-y divide-gray-200">
-              <thead class="bg-gray-50">
+      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
                 <tr>
                   {/* these are the same th, prolly better if we make things like this reusable considering we don't know what we'll be building in the future */}
                   <th
                     scope="col"
-                    class="px-8 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    className="px-8 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                   >
                     Name
                   </th>
                   <th
                     scope="col"
-                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                   >
                     Address
                   </th>
                   <th
                     scope="col"
-                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                   >
-                    Status
+                    Time
                   </th>
-                  <th scope="col" class="relative px-6 py-3">
-                    <span class="sr-only">Options</span>
+                  <th scope="col" className="relative px-6 py-3">
+                    <span className="sr-only">Options</span>
                   </th>
                 </tr>
               </thead>
-              <tbody class="bg-white divide-y divide-gray-200">
+              <tbody className="bg-white divide-y divide-gray-200">
                 {requestsData.map((data) => (
                   <RequestCards data={data} />
                 ))}
