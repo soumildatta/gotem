148039adf61c96e2d16c2581d6113335a9552898
Soumil Datta <sdatta2@cs.olemiss.edu>
2021-03-25 15:09:38 -0500

    Schedule frontend

--- src/pages/requestRide/RequestRide.jsx
+++ src/pages/requestRide/RequestRide.jsx
@@ -1,28 +1,26 @@
 import React from "react";
 
 // there's too much stuff in this file, should probably be broken out into separate components (but who cares tbh)
-// you are using 'class' in this file use 'className' instead
 const RequestRide = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen -mt-20 flex items-center justify-center bg-gray-50 px-4 sm:px-6 lg:px-8">
       <div className="max-w-5xl w-full space-y-10">
-        <h2 className="mt-6 text-center text-6xl font-extrabold text-gray-900 flex justify-left ">
+        <h2 className="ext-center text-6xl font-extrabold text-gray-900 flex justify-left ">
           Request a Ride
         </h2>
         <form className="mt-8 space-y-6" action="#" method="POST">
           <input type="hidden" name="remember" value="true" />
           <div className="rounded-md shadow-0 space-y-6">
             <div className="flex flex-col">
-              <label for="info" className="text-lg">
-                Info:
-              </label>
+              <label htmlFor="info">Info:</label>
               <select
+                defaultValue=""
                 name="info"
                 id="info"
-                class="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
               >
                 <optgroup label="Info">
-                  <option value="" disabled selected>
+                  <option value="" disabled>
                     (Optional)
                   </option>
                   <option value="fine">I'm probably fine</option>
@@ -34,31 +32,27 @@ const RequestRide = () => {
               </select>
             </div>
             <div>
-              <p className="text-lg">Your Location</p>
-              <label for="your-location" class="sr-only">
-                Your location
-              </label>
+              <label htmlFor="your-location">Your location</label>
               <input
                 id="your-location"
                 name="location"
                 type="text"
                 required
-                class="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
                 placeholder="Your location"
               ></input>
             </div>
 
             <div className="flex flex-col">
-              <label for="info" className="text-lg">
-                Available Hospitals:
-              </label>
+              <label htmlFor="info">Available Hospitals:</label>
               <select
+                defaultValue=""
                 name="available-hospitals"
                 id="available-hospitals"
-                class="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
               >
                 <optgroup label="Available Hospitals">
-                  <option value="" disabled selected>
+                  <option value="" disabled>
                     (Required)
                   </option>
                   <option value="bruh">bruh hospital</option>
@@ -68,16 +62,36 @@ const RequestRide = () => {
               </select>
             </div>
 
+            <div className="flex justify-between">
+              <div className="w-full mr-2">
+                <label htmlFor="date">Ride Date:</label>
+                <input
+                  type="date"
+                  id="date"
+                  className="w-full h-9 px-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                />
+              </div>
+              <div className="w-full ml-2">
+                <label htmlFor="time">Ride Time:</label>
+                <input
+                  type="time"
+                  id="time"
+                  className="w-full h-9 px-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                />
+              </div>
+            </div>
+
+            {/* Buttons */}
             <div className="flex justify-between py-2">
               <button
                 type="button"
-                class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-red-500 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 m-2"
+                className="group relative w-full py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-red-500 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 my-2 mr-2"
               >
                 Cancel
               </button>
               <button
                 type="submit"
-                class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-500 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 m-2"
+                className="group relative w-full py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-500 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 my-2 ml-2"
               >
                 Submit
               </button>
--- src/pages/requests/Requests.jsx
+++ src/pages/requests/Requests.jsx
@@ -1,60 +1,97 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import RequestCards from "./RequestCards";
+import AcceptedRequestCard from "./AcceptedRequestCard";
 
-// you are using 'class' in this file use 'className' instead
 const Requests = () => {
-  const requestsData = [
-    {
-      name: "Jane Cooper",
-      info: "Yikes, not good",
-      address: "3598 Mansion House Manor, Poor Place, MS 38655",
-      status: "Emergency",
-    },
-    {
-      name: "Joe Cooper",
-      info: "Ehh, aight.",
-      address: "3598 Mansion House Manor, Poor Place, MS 38655",
-      status: "Normal",
-    },
-  ];
+  const [rideStatus, setRideStatus] = useState("On My Way");
+
+  const requestsData = useMemo(() => {
+    return [
+      {
+        id: 1,
+        name: "Jane Cooper",
+        info: "Yikes, not good",
+        address: "3598 Mansion House Manor, Poor Place, MS 38655",
+        status: "Emergency",
+      },
+      {
+        id: 2,
+        name: "Joe Cooper",
+        info: "Ehh, aight.",
+        address: "3598 Mansion House Manor, Poor Place, MS 38655",
+        status: "Normal",
+      },
+    ];
+  }, []);
+
+  const rideRequest = {
+    time: "01/22/33 at 10:30am",
+    rideStatus: rideStatus,
+    name: "Smol Boi",
+    destination: "Large Hospital, Mippississi",
+    riderAddress: "Smol House place thing",
+  };
+
+  const handleStatusChange = (button) => {
+    if (button !== "Cancel") {
+      if (window.confirm(`Change status to "${button}"?`)) {
+        setRideStatus(button);
+        // firestore stuff here
+      }
+    } else {
+      // Request is cancelled by driver ??
+      if (
+        window.confirm("Are you sure you want to cancel this user's request?")
+      ) {
+        setRideStatus("Cancelled");
+      }
+    }
+  };
+
   return (
-    <div class="flex flex-col">
-      <h2 className="mt-6 text-center text-5xl font-medium ml-3 mb-4 text-gray-900 flex justify-left ">
+    <div className="flex flex-col">
+      <div className="my-8 mx-6 text-center">
+        <AcceptedRequestCard
+          request={rideRequest}
+          handleClick={handleStatusChange}
+        />
+      </div>
+      <h2 className="mt-6 text-center text-4xl font-medium ml-3 mb-4 text-gray-900 flex justify-left ">
         Requests
       </h2>
-      <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-          <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-            <table class="min-w-full divide-y divide-gray-200">
-              <thead class="bg-gray-50">
+      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
                 <tr>
                   {/* these are the same th, prolly better if we make things like this reusable considering we don't know what we'll be building in the future */}
                   <th
                     scope="col"
-                    class="px-8 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    className="px-8 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                   >
                     Name
                   </th>
                   <th
                     scope="col"
-                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                   >
                     Address
                   </th>
                   <th
                     scope="col"
-                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                   >
                     Status
                   </th>
-                  <th scope="col" class="relative px-6 py-3">
-                    <span class="sr-only">Options</span>
+                  <th scope="col" className="relative px-6 py-3">
+                    <span className="sr-only">Options</span>
                   </th>
                 </tr>
               </thead>
-              <tbody class="bg-white divide-y divide-gray-200">
+              <tbody className="bg-white divide-y divide-gray-200">
                 {requestsData.map((data) => (
-                  <RequestCards data={data} />
+                  <RequestCards key={data.id} data={data} />
                 ))}
               </tbody>
             </table>
