1d5ef5dfe57ab7c8fe9785042e1fb42e16c94e41
Soumil Datta <sdatta2@cs.olemiss.edu>
2021-03-25 18:36:52 -0500

    Revert "Merge branch 'authv1' into 'master'"
    This reverts merge request !18

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,23 @@
-import Home from "./pages/home/Home";
 import Signup from "./pages/signup/signup";
 import Signin from "./pages/signin/signin";
 import Requests from "./pages/requests/Requests";
 import RequestRide from "./pages/requestRide/RequestRide";
 import Home from "./pages/home/Home";
 import Navbar from "./shared/Navbar/Navbar";
-import Requests from "./pages/requests/Requests";
-import RequestRide from "./pages/requestRide/RequestRide";
-import UserDashboard from "./pages/userDashboard/UserDashboard";
 import DriverPayments from "./pages/driverPayments/DriverPayments";
+
 import ForgotPassword from "./pages/forgotPassword/ForgotPassword";
 
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { AuthProvider } from "./contexts/AuthContext";
+import UserDashboard from "./pages/userDashboard/UserDashboard";
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <div className="App overflow-x-hidden overflow-y-hidden">
-          <Navbar />
-        </div>
-
-        <Route exact path="/" component={Home} />
+    <Router>
+      <div className="App overflow-x-hidden overflow-y-hidden">
+        <Navbar />
+      </div>
 
       <Route exact path="/" component={Home} />
       <Route path="/signup" component={Signup} />
--- src/shared/FormButton.jsx
+++ src/shared/FormButton.jsx
@@ -5,7 +5,7 @@ const FormButton = ({ onClick, text, type = "submit" }) => {
     <button
       onClick={onClick}
       type={type}
-      className="group mt-2 relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-primaryBlue hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      class="group mt-2 relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-primaryBlue hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
     >
       {text}
     </button>
