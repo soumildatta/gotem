53e9e8791c4c0f35257aafeef0017bbee65d3133
Soumil Datta <sdatta2@cs.olemiss.edu>
2021-03-29 03:13:37 +0000

    Oui oui

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/auth";
+import "firebase/firestore";
 
 const app = firebase.initializeApp({
   apiKey: "AIzaSyABhHH_GRrIEzvOodEMlR3TqELB4xRMqHM",
@@ -11,4 +12,5 @@ const app = firebase.initializeApp({
 });
 
 export const auth = app.auth();
+export const db = app.firestore();
 export default app;
--- src/pages/requestRide/RequestRide.jsx
+++ src/pages/requestRide/RequestRide.jsx
@@ -1,19 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import { db } from "../../firebase";
+import { useAuth } from "../../contexts/AuthContext";
+import { useHistory } from "react-router";
 
 // there's too much stuff in this file, should probably be broken out into separate components (but who cares tbh)
 const RequestRide = () => {
+  const { currentUser } = useAuth();
+
+  const [input, setInput] = useState({
+    location: "",
+    hospital: "",
+    info: "",
+    date: "",
+    time: "",
+  });
+
+  const history = useHistory();
+  const goToDashboard = () => {
+    history.push("/dashboard");
+  };
+
+  const hasEmptyInput = () => {
+    return (
+      input.location !== "" &&
+      input.hospital !== "" &&
+      input.date !== "" &&
+      input.time !== ""
+    );
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (hasEmptyInput()) {
+      db.collection("Requests")
+        .add({
+          user: currentUser.email,
+          location: input.location,
+          hospital: input.hospital,
+          info: input.info,
+          date: input.date,
+          time: input.time,
+        })
+        .catch((error) => {
+          // prolly need a better way to handle the errors. or we could just make the users open up the console ya know
+          console.error("error occured: ", error);
+        });
+    }
+    goToDashboard();
+  };
+
   return (
     <div className="min-h-screen -mt-20 flex items-center justify-center bg-gray-50 px-4 sm:px-6 lg:px-8">
       <div className="max-w-5xl w-full space-y-10">
         <h2 className="ext-center text-6xl font-extrabold text-gray-900 flex justify-left ">
           Request a Ride
         </h2>
-        <form className="mt-8 space-y-6" action="#" method="POST">
+        <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
           <input type="hidden" name="remember" value="true" />
           <div className="rounded-md shadow-0 space-y-6">
             <div className="flex flex-col">
               <label htmlFor="info">Info:</label>
               <select
+                onChange={(e) => setInput({ ...input, info: e.target.value })}
                 defaultValue=""
                 name="info"
                 id="info"
@@ -34,11 +82,16 @@ const RequestRide = () => {
             <div>
               <label htmlFor="your-location">Your location</label>
               <input
+                onChange={(e) =>
+                  setInput({ ...input, location: e.target.value })
+                }
                 id="your-location"
                 name="location"
                 type="text"
                 required
-                className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                className={`appearance-none relative block w-full px-3 py-2 border ${
+                  input.location === "" ? "border-red-500" : "border-gray-300"
+                } placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm`}
                 placeholder="Your location"
               ></input>
             </div>
@@ -46,10 +99,15 @@ const RequestRide = () => {
             <div className="flex flex-col">
               <label htmlFor="info">Available Hospitals:</label>
               <select
+                onChange={(e) =>
+                  setInput({ ...input, hospital: e.target.value })
+                }
                 defaultValue=""
                 name="available-hospitals"
                 id="available-hospitals"
-                className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                className={`appearance-none relative block w-full px-3 py-2 border ${
+                  input.hospital === "" ? "border-red-500" : "border-gray-300"
+                } placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm`}
               >
                 <optgroup label="Available Hospitals">
                   <option value="" disabled>
@@ -66,17 +124,25 @@ const RequestRide = () => {
               <div className="w-full mr-2">
                 <label htmlFor="date">Ride Date:</label>
                 <input
+                  required
+                  onChange={(e) => setInput({ ...input, date: e.target.value })}
                   type="date"
                   id="date"
-                  className="w-full h-9 px-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                  className={`appearance-none relative block w-full px-3 py-2 border ${
+                    input.date === "" ? "border-red-500" : "border-gray-300"
+                  } placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm`}
                 />
               </div>
               <div className="w-full ml-2">
                 <label htmlFor="time">Ride Time:</label>
                 <input
+                  required
+                  onChange={(e) => setInput({ ...input, time: e.target.value })}
                   type="time"
                   id="time"
-                  className="w-full h-9 px-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                  className={`appearance-none relative block w-full px-3 py-2 border ${
+                    input.time === "" ? "border-red-500" : "border-gray-300"
+                  } placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm`}
                 />
               </div>
             </div>
@@ -84,6 +150,7 @@ const RequestRide = () => {
             {/* Buttons */}
             <div className="flex justify-between py-2">
               <button
+                onClick={goToDashboard}
                 type="button"
                 className="group relative w-full py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-red-500 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 my-2 mr-2"
               >
