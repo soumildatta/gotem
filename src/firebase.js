366230a175845a1d4a75a5fcf1282f5f10072d7a
Soumil Datta <sdatta2@cs.olemiss.edu>
2021-03-30 01:52:31 +0000

    Fetching requests for driver dashboard

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
-
 const app = firebase.initializeApp({
   apiKey: "AIzaSyABhHH_GRrIEzvOodEMlR3TqELB4xRMqHM",
   authDomain: "project-4cb84.firebaseapp.com",
--- src/pages/requests/RequestCards.jsx
+++ src/pages/requests/RequestCards.jsx
@@ -6,19 +6,19 @@ const RequestCards = ({ data }) => {
       <td className="px-4 py-4 whitespace-nowrap">
         <div className="flex items-center">
           <div className="ml-4">
-            <div className="text-sm font-medium text-gray-900">{data.name}</div>
+            <div className="text-sm font-medium text-gray-900">{data.user}</div>
             <div className="text-sm text-gray-500">{data.info}</div>
           </div>
         </div>
       </td>
       <td className="px-6 py-4 whitespace-nowrap">
-        <div className="text-sm text-gray-900">{data.address}</div>
+        <div className="text-sm text-gray-900">{data.location}</div>
       </td>
       <td className="px-6 py-4 whitespace-nowrap">
         {/* these are the same span, prolly better if we make things like this reusable considering we don't know what we'll be building in the future */}
         {/* also, best practice is to break this logic out into a function above and pass data.status as a param returning the correct span */}
         <span className="px-4 py-2 inline-flex text-xs leading-5 font-semibold rounded-full border border-black text-black">
-          {data.time}
+          {data.date} at {data.time}
         </span>
       </td>
       <td className="px-6 py-4 whitespace-nowrap">
