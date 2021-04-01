bcb62a501c9207f1bf83ad273708f9cadb9b08a3
Soumil Datta <sdatta2@cs.olemiss.edu>
2021-04-01 12:57:23 -0500

    Remove red borders

--- src/pages/requestRide/RequestRide.jsx
+++ src/pages/requestRide/RequestRide.jsx
@@ -93,9 +93,7 @@ const RequestRide = () => {
                 name="location"
                 type="text"
                 required
-                className={`appearance-none relative block w-full px-3 py-2 border ${
-                  input.location === "" ? "border-red-500" : "border-gray-300"
-                } placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm`}
+                className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
                 placeholder="Your location"
               ></input>
             </div>
@@ -109,9 +107,7 @@ const RequestRide = () => {
                 defaultValue=""
                 name="available-hospitals"
                 id="available-hospitals"
-                className={`appearance-none relative block w-full px-3 py-2 border ${
-                  input.hospital === "" ? "border-red-500" : "border-gray-300"
-                } placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm`}
+                className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
               >
                 <optgroup label="Available Hospitals">
                   <option value="" disabled>
@@ -132,9 +128,7 @@ const RequestRide = () => {
                   onChange={(e) => setInput({ ...input, date: e.target.value })}
                   type="date"
                   id="date"
-                  className={`appearance-none relative block w-full px-3 py-2 border ${
-                    input.date === "" ? "border-red-500" : "border-gray-300"
-                  } placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm`}
+                  className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
                 />
               </div>
               <div className="w-1/2 md:ml-2 min-w-min">
@@ -144,9 +138,7 @@ const RequestRide = () => {
                   onChange={(e) => setInput({ ...input, time: e.target.value })}
                   type="time"
                   id="time"
-                  className={`appearance-none relative block w-full px-3 py-2 border ${
-                    input.time === "" ? "border-red-500" : "border-gray-300"
-                  } placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm`}
+                  className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
                 />
               </div>
             </div>
