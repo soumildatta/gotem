fbfb48b9a7189461ddb552ab918cbf78b3706112
Soumil Datta <sdatta2@cs.olemiss.edu>
2021-04-20 11:14:02 -0500

    Fix timing thing

--- src/shared/RideInfoSkeleton.jsx
+++ src/shared/RideInfoSkeleton.jsx
@@ -17,16 +17,20 @@ const RideInfoSkeleton = ({
   handleWaiting,
 }) => {
   const formatTime = (time) => {
-    time = time
-      .toString()
-      .match(/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
+    if (time !== undefined) {
+      time = time
+        .toString()
+        .match(/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
 
-    if (time.length > 1) {
-      time = time.slice(1);
-      time[5] = +time[0] < 12 ? "AM" : "PM";
-      time[0] = +time[0] % 12 || 12;
+      if (time.length > 1) {
+        time = time.slice(1);
+        time[5] = +time[0] < 12 ? "AM" : "PM";
+        time[0] = +time[0] % 12 || 12;
+      }
+      return time.join("");
+    } else {
+      return time;
     }
-    return time.join("");
   };
 
   // Drivers and users have different info rideInfo
