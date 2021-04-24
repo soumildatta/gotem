93810869b4b67bf285f377692f75bcb2a604235a
Farid Salau <fsalau1@go.olemiss.edu>
2021-04-23 20:43:41 -0500

    dfs

--- src/pages/requests/RequestCards.jsx
+++ src/pages/requests/RequestCards.jsx
@@ -54,8 +54,6 @@ const RequestCards = ({ data, acceptRequest, disableButton }) => {
         <div className="text-sm text-gray-900">{data.location}</div>
       </td>
       <td className="px-6 py-4 whitespace-nowrap">
-        {/* these are the same span, prolly better if we make things like this reusable considering we don't know what we'll be building in the future */}
-        {/* also, best practice is to break this logic out into a function above and pass data.status as a param returning the correct span */}
         <span className="px-4 py-2 inline-flex text-xs leading-5 font-semibold rounded-full border border-black text-black">
           {data.date} at {formatTime(data.time)}
         </span>
